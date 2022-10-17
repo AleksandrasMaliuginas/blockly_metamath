81b9f4e71cc3586cb9c1f9d0c0cf747dab419bef
Aleksandras <aleksandrasm348@gmail.com>
2022-10-17 09:54:13 +0300

    add variable blocks

--- src/components/toolbox/blockTemplates.js
+++ src/components/toolbox/blockTemplates.js
@@ -20,6 +20,26 @@ export const blocks = [
     "output": null,
     "colour": 160,
   },
+  {
+    "type": "mm_variable",
+    "message0": '%1 %2',
+    "args0": [
+      {
+        "type": "field_variable",
+        "name": "VAR",
+        "variable": "%{BKY_VARIABLES_DEFAULT_NAME}",
+        "variableTypes": ["variable"], // Specifies what types to put in the dropdown
+        "defaultType": "variable"
+      },
+      {
+        "type": "input_value",
+        "name": "VALUE",
+        "check": "String"
+      }
+    ],
+    "output": null,
+    "colour": 130,
+  },
   
 ]
 
@@ -31,6 +51,12 @@ export const toolbox = {
       "name": "Constants",
       "colour": 160,
       "custom": "MM_CONSTANTS"
+    },
+    {
+      "kind": "category",
+      "name": "Variables",
+      "colour": 130,
+      "custom": "MM_VARIABLES"
     },
   ]
 };
